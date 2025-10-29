633cea8fd78a90c05c69705e9b16983705ce09c6
KEERTHIAANAND <keerthiaanand77@gmail.com>
2025-10-29 10:25:52 +0530

    Added initial entity, repository, service, and controller classes; configure application properties added and completed user.java

---
+++ JavaProject/src/main/java/com/projectleap/JavaProject/entity/User.java
@@ -0,0 +1,24 @@
+package com.projectleap.javaproject.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import lombok.AllArgsConstructor; //constructor with all arguments
+import lombok.NoArgsConstructor; //constructor without arguments
+
+@Entity
+@Data
+@AllArgsConstructor 
+@NoArgsConstructor
+
+public class User{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY) //auto id generation
+    private Long userId;
+    private String name;
+    private String email;
+    private String role; //staffs, supervisors, admins
+    private String password;
+}
